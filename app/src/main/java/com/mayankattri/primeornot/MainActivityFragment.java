bb299a0c28cbd8411e638224a81871eab8938d5f
a3mayank <a3mayank@gmail.com>
2016-08-26 21:32:35 +0530

    Added more functionality

--- app/src/main/java/com/mayankattri/primeornot/MainActivityFragment.java
+++ app/src/main/java/com/mayankattri/primeornot/MainActivityFragment.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -27,20 +26,20 @@ public class MainActivityFragment extends Fragment {
     private int randomInt;
     private int hintFlag;
     private int cheatFlag;
+    private int image_index = 0; // index for images arraylist
+
     private TextView question_View;
+    private TextView TVinfo;
+
     private String question_String;
+    private String MESSAGE = "MainActivityFragment : ";
+
     private static final String CORRECT_ANSWER = "Correct Answer :)";
     private static final String INCORRECT_ANSWER = "Incorrect Answer :(";
     private static final String CURRENT_VALUE = "Current Value";
     private static final String CURRENT_BG_INDEX = "Current BG Index";
-    private String MESSAGE = "MainActivityFragment : ";
-    // ArrayList of images for background.
-    ArrayList<Integer> myImageList = new ArrayList<>();
-    // index for images arraylist
-    private int image_index = 0;
 
-    private Button buttonHint;
-    private Button buttonCheat;
+    ArrayList<Integer> myImageList = new ArrayList<>(); // ArrayList of images for background.
 
     public MainActivityFragment() {
     }
@@ -122,8 +121,10 @@ public class MainActivityFragment extends Fragment {
         Button buttonYes = (Button) rootView.findViewById(R.id.B_correct);
         Button buttonNo = (Button) rootView.findViewById(R.id.B_incorrect);
         Button buttonNext = (Button) rootView.findViewById(R.id.B_next);
-        buttonHint = (Button) rootView.findViewById(R.id.B_hint);
-        buttonCheat = (Button) rootView.findViewById(R.id.B_cheat);
+        Button buttonHint = (Button) rootView.findViewById(R.id.B_hint);
+        Button buttonCheat = (Button) rootView.findViewById(R.id.B_cheat);
+
+        TVinfo = (TextView) rootView.findViewById(R.id.TV_info);
 
         rootView.setBackgroundResource(myImageList.get(image_index++));
 
@@ -169,8 +170,11 @@ public class MainActivityFragment extends Fragment {
                 rootView.setBackgroundResource(myImageList.get(image_index));
                 image_index++;
 
-                buttonHint.setEnabled(true);
-                buttonCheat.setEnabled(true);
+                TVinfo.setText("");
+
+                hintFlag = 0;
+                cheatFlag = 0;
+
             }
         });
 
@@ -257,28 +261,36 @@ public class MainActivityFragment extends Fragment {
         Log.d("requestCode ", Integer.toString(requestCode));
         Log.d("resultCode ", Integer.toString(resultCode));
 
+        String hint = "Hint Used";
+        String cheat = "Cheat Used";
+        String both = "Both Hint and Cheat Used";
+
         if(resultCode == Activity.RESULT_OK && requestCode == 1) {
             if(data != null) {
                 String value = data.getStringExtra("hintActivity");
                 hintFlag = Integer.parseInt(value);
                 if (hintFlag == 1) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Hint Taken", Toast.LENGTH_SHORT)
-                            .show();
-                    buttonHint.setEnabled(false);
+//                    Toast.makeText(getActivity().getApplicationContext(), "Hint Taken", Toast.LENGTH_SHORT)
+//                            .show();
+                    TVinfo.setText(hint);
                 }
             }
         }
+
         if(resultCode == Activity.RESULT_OK && requestCode == 2) {
             if(data != null) {
                 String value = data.getStringExtra("cheatActivity");
                 cheatFlag = Integer.parseInt(value);
                 if (cheatFlag == 1) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Cheat Taken", Toast.LENGTH_SHORT)
-                            .show();
-                    buttonHint.setEnabled(false);
-                    buttonCheat.setEnabled(false);
+//                    Toast.makeText(getActivity().getApplicationContext(), "Cheat Taken", Toast.LENGTH_SHORT)
+//                            .show();
+                    TVinfo.setText(cheat);
                 }
             }
+        }
+
+        if(hintFlag == 1 && cheatFlag == 1) {
+            TVinfo.setText(both);
         }
 
         Log.d("hintFlag ", Integer.toString(hintFlag));
--- app/src/main/java/com/mayankattri/primeornot/SplashScreen.java
+++ app/src/main/java/com/mayankattri/primeornot/SplashScreen.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 10000;
+    private String MESSAGE = "SplashScreen : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(MESSAGE, " OnCreate invoked");
+
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
@@ -26,7 +30,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
 
                 // close this activity
